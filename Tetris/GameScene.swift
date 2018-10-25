9a0a9efc178741e0f86829cc311ab82602fb2eec
Connor Ng <connor.z.ng@gmail.com>
2018-10-24 21:01:30 -0700

    .

--- Tetris/GameScene.swift
+++ Tetris/GameScene.swift
@@ -9,12 +9,18 @@
 import SpriteKit
 
 let TickLengthLevelOne = TimeInterval(600)
+let BlockSize:CGFloat = 20.0
 
 class GameScene: SKScene {
 	
+    let gameLayer = SKNode ()
+    let shapeLayer = SKNode ()
+    let layerPosition = CGPoint (x: 6, y: -6)
+    
 	var tick:(() -> ())?
 	var tickLengthMillis = TickLengthLevelOne
 	var lastTick:NSDate?
+    var textureCache = Dictionary <String, SKTexture> ()
 	
 	required init(coder aDecoder: NSCoder) {
 		fatalError("NSCoder not supported")
@@ -29,7 +35,12 @@ class GameScene: SKScene {
 		background.position = CGPoint(x: 0, y: 0)
 		background.anchorPoint = CGPoint(x: 0, y: 1.0)
 		addChild(background)
-	}
+        
+        addChild(gameLayer)
+        let gameboardTexture = SKTexture (imageNamed: "gameboard")
+        let gameboard = SKSpriteNode (texture: gameboardTexture, size: CGSizeMake (BlockSize * CGFloat(NumColumns), BlockSize * CGFloat(NumRows)) )
+        
+    }
 	
     override func update(_ currentTime: TimeInterval) {
 		guard let lastTick = lastTick else {
--- Tetris/Tetris.swift
+++ Tetris/Tetris.swift
@@ -6,4 +6,31 @@
 //  Copyright © 2018 Valley Tutoring. All rights reserved.
 //
 
-import Foundation
+let NumColumns = 10
+let NumRows = 20
+let StartingColumn = 4
+let StartingRow = 0
+let PreviewColumn = 12
+let PreviewRow = 1
+
+class Tetris {
+    var blockArray: Array2D <Block>
+    var nextShape: Shape?
+    var fallingShape: Shape?
+    init (){
+        fallingShape = nil
+        nextShape = nil
+        blockArray = Array2D <Block>(columns: NumColumns, rows: NumRows)
+    }
+    func beginGame () {
+        if (nextShape == nil) {
+            nextShape = Shape.random (startingColumn: PreviewColumn, startingRow: PreviewRow)
+        }
+    }
+    func newShape() -> (fallingShape: Shape?, nextShape: Shape?) {
+        fallingShape = nextShape
+        nextShape = Shape.random(startingColumn: PreviewColumn, startingRow: PreviewRow)
+        fallingShape?.moveTo(column: StartingColumn, row: StartingRow)
+        return (fallingShape, nextShape)
+    }
+}
