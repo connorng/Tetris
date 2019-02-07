9d66d8b25889da0f3aabcfd647bafb9a9594e0f5
Connor Ng <connor.z.ng@gmail.com>
2019-02-06 20:59:06 -0800

    StartedWorkingOnStoryBoard

--- Tetris/GameViewController.swift
+++ Tetris/GameViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, TetrisDelegate, UITapGestureRecognizer {
 
 	var scene: GameScene!
     var tetris: Tetris!
+    var panPointReference: CGPoint?
 	
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,27 +29,94 @@ class GameViewController: UIViewController {
 		
         scene.tick = didTick;
         tetris = Tetris()
+        tetris.delegate = self
         tetris.beginGame()
         
 		// Present the scene.
 		skView.presentScene(scene)
-        scene.addPreviewShapeToScene(shape: tetris.nextShape!) {
-            self.tetris.nextShape?.moveTo(column: StartingColumn, row: StartingRow)
-            self.scene.movePreviewShape(shape: self.tetris.nextShape!){
-                let nextShape = self.tetris.newShape()
-                self.scene.startTicking()
-                self.scene.addPreviewShapeToScene(shape: nextShape.nextShape!, completion: {})
-            }
-        }
     }
 	
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
+    @IBAction func didSwipe(_ sender: UISwipeGestureRecognizer) {
+        tetris.dropShape()
+    }
+    func gestureRecognizer() -> Bool {
+        return true
+    }
+    func gestureRecognizer() -> Bool {
+        
+    }
+    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
+        tetris.rotateShape()
+    }
+    
+    @IBAction func didPan(_ sender: UIPanGestureRecognizer) {
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9){
+                if sender.velocityInView(self.view).x > CGFloat(0){
+                    tetris.moveShapeRight()
+                    panPointReference = currentPoint
+                }
+                else{
+                    tetris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        }
+        else if sender.state == .Began{
+            panPointReference = currentPoint
+        }
+    }
     func didTick(){
+        tetris.letShapeFall()
         tetris.fallingShape?.lowerShapeByOneRow()
-        scene.redrawShape(tetris.fallingShape!){}
+        scene.redrawShape(tetris.fallingShape!, {})
+    }
+    func nextShape(){
+        let newShape = tetris.nextShape()
+        guard let fallingShape = newShape.fallingShape else {
+            return
+        }
+        self.scene.addPreviewShapeToScene(shape: newShapes.nextShape, completion: {})
+        self.scene.movePreviewShape(shape: fallingShape, completion: {
+            self.view.isUserInteractionEnabled = true
+            self.scene.startTicking()
+        })
+    }
+    func gameDidBegin(tetris: Tetris) {
+        if tetris.nextShape != nil && tetris.nextShape!.blocks[0].sprite == nil{
+            scene.addPreviewShapeToScene(shape: tetris.nextShape!, completion: {
+                self.nextShape()
+            })
+        }
+        else {
+            nextShape()
+        }
+    }
+    func gameDidEnd(tetris: Tetris) {
+        view.isUserInteractionEnabled = false
+        scene.stopTicking()
+    }
+    func gameDidLevelUp(tetris: Tetris) {
+    }
+    func gameShapeDidDrop(tetris: Tetris) {
+        scene.stopTicking()
+        scene.redrawShape(tetris.fallingShape!, {
+            tetris.letShapeFall()
+            
+        })
+        
+    }
+    func gameShapeDidLand(tetris: Tetris) {
+        scene.stopTicking()
+        nextShape()
+    }
+    func gameShapeDidMove(tetris: Tetris) {
+        scene.redrawShape(tetris.fallingShape!, {})
     }
 }
 
